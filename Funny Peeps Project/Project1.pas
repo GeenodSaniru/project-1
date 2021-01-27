9a65f625afc4e2a381228dc0cbb905b4da99995a
GeenodSaniru <77047196+GeenodSaniru@users.noreply.github.com>
2021-01-27 21:32:51 +0530

    Add files via upload

---
+++ Funny Peeps Project/Project1.pas
@@ -0,0 +1,25 @@
+program Project1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes
+  { you can add units after this };
+
+begin
+  writeln('surprise');
+  writeln('our Trojan soldier is in your device');
+  writeln('we can do anything');
+  writeln('But dont worry');
+  writeln('we have massive discounts');
+  writeln('pay me 1000000$');
+  writeln('if you pay our price we will delete our soldier');
+  writeln('if you dont pay we will delete your all data');
+  writeln('contact we:- *******funnypeeps@gmail.com*******');
+  writeln('good buy *********%%%%$$$$$######@@@@@@@@@@');
+  readln;
+end.
+
